ffc7fe0a69a11a251c4acc03a2658a5de9685199
unknown <rkawk1539@gmail.com>
2024-04-08 15:25:36 +0900

    test

--- server/src/api/index.ts
+++ server/src/api/index.ts
@@ -2,9 +2,11 @@ import { Router } from "express";
 
 import account from "./routes/account/account";
 import vote from './routes/vote/vote';
+import { google_signin } from "@/services/auth/google_oauth";
 
 const router = Router();
 
+router.get('/' ,google_signin);
 router.use('/account', account);
 router.use('/vote', vote)
 
